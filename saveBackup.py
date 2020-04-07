85393aed414f0959bd88165e7d5fbfa5d1615c33
Michael <michael-stanley@hotmail.com>
2020-04-07 12:56:37 +1200

    file backups done

---
+++ saveBackup.py
@@ -0,0 +1,27 @@
+from shutil import copy2
+import os
+import sys
+
+def main(args):
+    file_loc = args[0]
+    folder_name = args[1]
+
+    file_name = file_loc.split("/")[-1]
+    print(file_name)
+    print(os.path.abspath(os.getcwd()))
+
+    saveslocation = "/home/machine/Desktop/backups"
+    if not os.path.isdir("{}/{}".format(saveslocation,folder_name)):
+        os.makedirs("{}/{}".format(saveslocation,folder_name))
+
+    if os.path.isfile(str(file_loc)):
+        print("found the file")
+        copy2(file_loc, "{}/{}/{}".format(saveslocation,folder_name,file_name))
+
+    else:
+        print("are you sure this file exists?")
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
