b705edb68116bf8bd2318293ba859066e7009573
Michael <michael-stanley@hotmail.com>
2020-04-07 15:52:28 +1200

    passing values creates json file that stores all saveable files & saves passed file. passing no values saves all files in stored json file

--- saveBackup.py
+++ saveBackup.py
@@ -1,27 +1,70 @@
 from shutil import copy2
 import os
 import sys
+import json
 
-#backup a file by passing {original file location} {folder for copied file}
+#backup a file by passing {original file location} {folder for copied file to be placed in}
+#passed files are stored in a dictionary text file
 def main(args):
-    file_loc = args[0]
-    folder_name = args[1]
 
+    if len(args) > 0:
+        file_loc = args[0]
+        folder_name = args[1]
+        savefile(file_loc, folder_name)
+        writeJSON(file_loc, folder_name)
+
+    else:
+        with open('data.txt') as json_file:
+            data = json.load(json_file)
+            for p in data['saves']:
+                savefile(p['location'], p['folder'])
+
+    #copy the file
+    # savefile(file_loc, folder_name)
+
+
+
+    #write the files details to json
+    # writeJSON(data, file_loc, folder_name)
+
+
+
+def savefile(file_loc, folder_name):
+    
     file_name = file_loc.split("/")[-1]
     print(file_name)
     print(os.path.abspath(os.getcwd()))
-
+    # could be improved to only check for the folder name in the location 
     saveslocation = "/home/machine/Desktop/backups"
     if not os.path.isdir("{}/{}".format(saveslocation,folder_name)):
         os.makedirs("{}/{}".format(saveslocation,folder_name))
 
+    # checking if the file exists
     if os.path.isfile(str(file_loc)):
         print("found the file")
         copy2(file_loc, "{}/{}/{}".format(saveslocation,folder_name,file_name))
 
     else:
-        print("are you sure this file exists?")
-    
+        print("are you sure {} exists?").format(str(file_loc))
+
+def writeJSON(file_loc, folder_name):
+    try:
+        with open('data.txt') as json_file:
+            data = json.load(json_file)
+    except ValueError:
+        print('no backups dictionary found, now creating one')
+        data = {}
+        data['saves'] = []
+
+        
+    data['saves'].append({
+        'location' : str(file_loc),
+        'folder' : str(folder_name)
+        })
+
+    with open('data.txt', 'w') as json_file:
+        json.dump(data, json_file)
+        
 
 if __name__ == "__main__":
     main(sys.argv[1:])
